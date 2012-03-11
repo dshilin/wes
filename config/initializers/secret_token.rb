a56ad0c514b27253c9421bb22d9b56058918647e
Dmitry Shilin <dmitryshilin@agileorange.ru>
2012-03-11 21:29:40 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wes::Application.config.secret_token = '7eecfabeb4095052ff6f32c0ab99d683e19e7f769173578eec6e74255e3b64ac1c50ba32a08459a38e554232614a4d6317b41b6a1dfaa382a53066b92b5e8370'
